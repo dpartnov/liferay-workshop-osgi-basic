c3161684be1bdc2dff08b624e579fa2d3a2d2bb7
denys.partnov <denys.partnov@lundegaard.eu>
2024-10-15 17:37:57 +0100

    hello world component

--- portal/modules/hello-world-api/src/main/java/com/liferay/workshop/helloworld/api/impl/HelloWorldApiImpl.java
+++ portal/modules/hello-world-api/src/main/java/com/liferay/workshop/helloworld/api/impl/HelloWorldApiImpl.java
@@ -1,11 +1,16 @@
 package com.liferay.workshop.helloworld.api.impl;
 
 import com.liferay.workshop.helloworld.api.HelloWorldApi;
+import org.osgi.service.component.annotations.Component;
 
+@Component(
+        property = {"osgi.command.function=hello", "osgi.command.scope=hello-world-api"},
+        service = HelloWorldApi.class
+)
 public class HelloWorldApiImpl implements HelloWorldApi {
 
     @Override
     public void hello(String name) {
-        System.out.println("Hello " + name);
+        System.out.printf("Hello %s !", name);
     }
 }
