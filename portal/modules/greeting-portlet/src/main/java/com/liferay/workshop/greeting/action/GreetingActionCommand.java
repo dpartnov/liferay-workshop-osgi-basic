3f747e5694a23620eb5cfa9046075a6b88c29731
denys.partnov <denys.partnov@lundegaard.eu>
2024-10-17 08:57:46 +0200

    Refactoring

--- portal/modules/greeting-portlet/src/main/java/com/liferay/workshop/greeting/action/GreetingActionCommand.java
+++ portal/modules/greeting-portlet/src/main/java/com/liferay/workshop/greeting/action/GreetingActionCommand.java
@@ -4,7 +4,9 @@ import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.workshop.greeting.constants.GreetingPortletKeys;
+import com.liferay.workshop.helloworld.api.HelloWorldApi;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
@@ -21,12 +23,15 @@ import static com.liferay.workshop.greeting.constants.GreetingPortletKeys.ATTR_U
         service = MVCActionCommand.class)
 public class GreetingActionCommand extends BaseMVCActionCommand {
 
+    @Reference
+    private HelloWorldApi helloWorldApi;
+
     @Override
     protected void doProcessAction(ActionRequest actionRequest, ActionResponse actionResponse) throws Exception {
 
         String userName = ParamUtil.get(actionRequest, ATTR_USER_NAME, "John Doe");
-        System.out.println("USER NAME: " + userName);
+        String result = helloWorldApi.hello(userName);
 
-        actionRequest.setAttribute(ATTR_RESULT, userName);
+        actionRequest.setAttribute(ATTR_RESULT, result);
     }
 }
